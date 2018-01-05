d843b46b8c60484ea8948590ddddeacd0067dea8
unknown <wahyuros@PB90048.telkomsel.co.id>
2018-01-05 16:17:48 +0700

    finished chapter 6

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -22,6 +22,5 @@ def hours_ahead(request,offset):
     except ValueError:
         raise Http404()
     dt = datetime.datetime.now()+datetime.timedelta(hours=offset)
-    html = '<html><body>In %s hour(s), it will be %s.</body></html>'\
-        %(offset,dt)
-    return HttpResponse(html)
+    return render(request,'hours_ahead.html',\
+		{'hours_offset':offset,'next_time':dt})
